25264fd77458c9d42bc7af746ff84c98da639d54
Jay Cha <kimchikimchi@gmail.com>
2018-10-08 16:03:04 -0400

    - Changed the table name fromt table to tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,12 @@
 -- Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
-INSERT INTO burger (burger_name) VALUES ("Plain Burger");
-INSERT INTO burger (burger_name) VALUES ("Chesse Burger");
-INSERT INTO burger (burger_name) VALUES ("Bacon Burger");
-INSERT INTO burger (burger_name) VALUES ("Bacon Cheese  Burger");
-INSERT INTO burger (burger_name) VALUES ("BBQ Chicken Burger");
-INSERT INTO burger (burger_name) VALUES ("Pork Burger");
-INSERT INTO burger (burger_name) VALUES ("Fried Chicken Burger");
-INSERT INTO burger (burger_name) VALUES ("Grilled Chicken Burger");
-INSERT INTO burger (burger_name) VALUES ("Double Beef Patty Burger");
-INSERT INTO burger (burger_name) VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Plain Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chesse Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese  Burger");
+INSERT INTO burgers (burger_name) VALUES ("BBQ Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Pork Burger");
+INSERT INTO burgers (burger_name) VALUES ("Fried Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Grilled Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Beef Patty Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
