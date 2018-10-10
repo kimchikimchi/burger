0f663005382a7a78f562c0a5f3f768fabad94c35
Jay Cha <kimchikimchi@gmail.com>
2018-10-10 14:56:52 -0400

    - Slightly modified seeds to have at least one row of devoured set to   true

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 -- Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
 
-INSERT INTO burgers (burger_name) VALUES ("Plain Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Plain Burger", true);
 INSERT INTO burgers (burger_name) VALUES ("Chesse Burger");
 INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
 INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese  Burger");
