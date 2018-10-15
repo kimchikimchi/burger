9e9eeeaadeee58e42284688eff9ea9233c142286
Jay Cha <kimchikimchi@gmail.com>
2018-10-15 14:17:45 -0400

    - Removed duplicate function.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,21 +1,5 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(document).ready( function() {
-    $(".change-sleep").on("click", function(event) {
-        var id = $(this).data("id");
-
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: { devoured: $(this).data('devoured') }
-        }).then(
-            function() {
-                console.log("changed sleep to", data);
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
-
     $(".create-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
@@ -33,8 +17,8 @@ $(document).ready( function() {
         );
     });
 
+    // Using event delegation since ".btn" can be created after page rendering.
     $("#uneatenBurgers").on("click", ".btn", function(event) {
-        event.preventDefault();
         var id = $(this).data("id");
         console.log("updating devoured status for id " + id);
 
