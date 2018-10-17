1d555e04f48089f2a2e790b6556e9343359a90a8
Jay Cha <kimchikimchi@gmail.com>
2018-10-16 20:51:03 -0400

    - Added Heroku MySQL add-on connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ Inside the connection.js file, setup the code to connect Node to MySQL.
 Export the connection.
 */
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_UR);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
 connection.connect(function(err) {
